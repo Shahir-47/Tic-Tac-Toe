7108753ba86a852f916979613669253e39d61590
Shahir Ahmed <ahmeds@dickinson.edu>
2023-06-22 01:28:41 -0400

    seems to work

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const Gameboard = (() => {
-    let boardArray = [['X','X','X'], ['O', 'O', 'O'], ['X','X','X']]
+    let boardArray = [['','',''], ['', '', ''], ['','','']]
     const getBoard = () => board;
     const setBoard = (newBoard) => board = newBoard;
     const resetBoard = () => board = [['','',''], ['','',''], ['','','']];
@@ -20,7 +20,7 @@ const Gameboard = (() => {
 
     const updateBoard = () => {
         const squares = document.querySelectorAll('.square');
-        squares.forEach((square) => {
+        squares.forEach((square, i) => {            
             square.textContent = boardArray[Math.floor(i/3)][i%3];
         })
     }
@@ -28,21 +28,21 @@ const Gameboard = (() => {
     const checkWin = () => {
         // check rows
         for (let i = 0; i < 3; i++) {
-            if (boardArray[i][0] === boardArray[i][1] && boardArray[i][1] === boardArray[i][2]) {
+            if (boardArray[i][0] === boardArray[i][1] && boardArray[i][1] === boardArray[i][2] && boardArray[i][0] !== '') {
                 return boardArray[i][0];
             }
         }
         // check columns
         for (let i = 0; i < 3; i++) {
-            if (boardArray[0][i] === boardArray[1][i] && boardArray[1][i] === boardArray[2][i]) {
+            if (boardArray[0][i] === boardArray[1][i] && boardArray[1][i] === boardArray[2][i] && boardArray[0][i] !== '') {
                 return boardArray[0][i];
             }
         }
         // check diagonals
-        if (boardArray[0][0] === boardArray[1][1] && boardArray[1][1] === boardArray[2][2]) {
+        if (boardArray[0][0] === boardArray[1][1] && boardArray[1][1] === boardArray[2][2] && boardArray[0][0] !== '') {
             return boardArray[0][0];
         }
-        if (boardArray[0][2] === boardArray[1][1] && boardArray[1][1] === boardArray[2][0]) {
+        if (boardArray[0][2] === boardArray[1][1] && boardArray[1][1] === boardArray[2][0] && boardArray[0][2] !== '') {
             return boardArray[0][2];
         }
         return false;
@@ -71,6 +71,7 @@ const Gameboard = (() => {
         if (boardArray[Math.floor(square/3)][square%3] === '') {
             boardArray[Math.floor(square/3)][square%3] = player.mark;
             updateBoard();
+            console.log("line 74:" + gameController.gameOver)
             return checkEnd();
         }
         return false;
@@ -84,9 +85,50 @@ const player = (name, mark) => {
     return {name, mark}
 }
 
-const displayController = (() => {
+// const displayController = (() => {
+//     console.log('displayController');
+//     Gameboard.displayBoard();
+//     const squares = document.querySelectorAll('.square');
+//     squares.forEach((square) => {
+//         square.addEventListener('click', (e) => {
+//             if (!gameController.gameOver) {
+//                 gameController.playRound(e.target.id.slice(-1));
+//                 console.log("game over: " + gameController.gameOver);
+                
+//             }
+//         })
+//     })
+
+// })();
+
+const gameController = (() => {
+    Gameboard.displayBoard();
+    const player1 = player('Player 1', 'X');
+    const player2 = player('Player 2', 'O');
+    let currentPlayer = player1;
+    let result = null;
+    let gameOver = false;
+  
+    const squares = document.querySelectorAll('.square');
+    squares.forEach((square) => {
+      square.addEventListener('click', (e) => {
+        let state = !gameOver;
+        console.log("line 117: " + state);
+        if (state) {
+          result = Gameboard.playRound(currentPlayer, e.target.id.slice(-1));
+          if (result) {
+            gameOver = true;
+          }
+          if (currentPlayer === player1) {
+            currentPlayer = player2;
+          } else {
+            currentPlayer = player1;
+          }
+          console.log("game over: " + gameOver);
+        }
+      });
+    });
+  
+    return { result, currentPlayer };
 })();
-
-const gameController = (() => {})();
-
-Gameboard.displayBoard();
+  

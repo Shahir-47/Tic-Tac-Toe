e1929df978430afa971869a5618b08b4348b9e0a
Shahir Ahmed <ahmeds@dickinson.edu>
2023-06-21 21:51:40 -0400

    added some more functions

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ const Gameboard = (() => {
     const getBoard = () => board;
     const setBoard = (newBoard) => board = newBoard;
     const resetBoard = () => board = [['','',''], ['','',''], ['','','']];
+    
     const displayBoard = () => {
-        const container = document.querySelector('.container');
+        const boardContainer = document.querySelector('.board-container');
         const board = document.createElement('div');
         board.classList.add('board');
-        container.appendChild(board);
+        boardContainer.appendChild(board);
         for (let i = 0; i < 9; i++) {
             const square = document.createElement('div');
             square.classList.add('square');
@@ -16,7 +17,67 @@ const Gameboard = (() => {
             board.appendChild(square);
         }
     }
-    return {getBoard, setBoard, resetBoard, displayBoard}
+
+    const updateBoard = () => {
+        const squares = document.querySelectorAll('.square');
+        squares.forEach((square) => {
+            square.textContent = boardArray[Math.floor(i/3)][i%3];
+        })
+    }
+
+    const checkWin = () => {
+        // check rows
+        for (let i = 0; i < 3; i++) {
+            if (boardArray[i][0] === boardArray[i][1] && boardArray[i][1] === boardArray[i][2]) {
+                return boardArray[i][0];
+            }
+        }
+        // check columns
+        for (let i = 0; i < 3; i++) {
+            if (boardArray[0][i] === boardArray[1][i] && boardArray[1][i] === boardArray[2][i]) {
+                return boardArray[0][i];
+            }
+        }
+        // check diagonals
+        if (boardArray[0][0] === boardArray[1][1] && boardArray[1][1] === boardArray[2][2]) {
+            return boardArray[0][0];
+        }
+        if (boardArray[0][2] === boardArray[1][1] && boardArray[1][1] === boardArray[2][0]) {
+            return boardArray[0][2];
+        }
+        return false;
+    }
+
+    const checkTie = () => {
+        for (let i = 0; i < 3; i++) {
+            if (boardArray[i].includes('')) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    const checkEnd = () => {
+        if (checkWin()) {
+            return checkWin();
+        }
+        if (checkTie()) {
+            return 'tie';
+        }
+        return false;
+    }
+
+    const playRound = (player, square) => {
+        if (boardArray[Math.floor(square/3)][square%3] === '') {
+            boardArray[Math.floor(square/3)][square%3] = player.mark;
+            updateBoard();
+            return checkEnd();
+        }
+        return false;
+    }
+
+    return {displayBoard, playRound}
+
 })();
 
 const player = (name, mark) => {
